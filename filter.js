bddf431230be11ff805cb838b425e0477f34c157
Boinx Bantug <boinxbantug25@yahoo.com>
2013-12-10 21:57:54 +0800

    done.

--- filter.js
+++ filter.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){  
 
    var i=4;
-   var boinxArray = ["Efren","Gamotea","Bantug","Lim"];
+   var boinxArray = ["Boinx","Gamotea","Bantug","Bro"];
    $("#words").html(boinxArray + "\xa0");
+   var res= [];
  
    function filterLongWords(myArray){
    		for(var a = 0; a < myArray.length ;a++) {
       
        	if (myArray[a].length>i){
 
-           	return myArray[a];
-           
+           	res.push(myArray[a] + "\xa0");
+              
+   		  }
    		}
-   		
-  	 }
+
+      return res;
     }
 
 	$("#filter").click(function(){	
